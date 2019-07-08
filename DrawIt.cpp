3a95d3f9ee012f731b4009d626fc8d39886ec887
hyp19991114 <47892305+hyp19991114@users.noreply.github.com>
2019-07-08 09:47:59 +0800

    代码最终版

--- DrawIt.cpp
+++ DrawIt.cpp
@@ -1,9 +1,9 @@
 ﻿/* *******************************************************
 |               面向对象程序设计综合应用
-|     选    题  画图软件的设计与实现
+|     选    题  画图软件的设计与实现 Draw It
 |     姓    名  黄远鹏      学    号  183401050225
 |     班    级  软件1802
-|     完成时间  2019年7月3日
+|     完成时间  2019年7月7日
 \  ******************************************************* */
 #include <iostream>
 #include <fstream>
@@ -13,15 +13,16 @@
 #include <cmath>
 #include <vector>
 #include <string>
+#include <regex>
 using namespace std;
 
 const double PI=acos(-1.0);     // PI 圆周率
 // equals0：判断浮点数是否为0     返回值：true（为0），false（非0）
-bool equals0(double num)
+inline bool equals0(double num)
 {
 	return fabs(num) < 1e-8;
 }
-class Shape     // Shape类：抽象类，作为其他类的基类
+class Shape     // Shape类：作为其他类的基类
 {
 protected:
 	double perimeter,area;     // perimeter 周长,area 面积
@@ -35,10 +36,9 @@ public:
 		os << "周长 = " << perimeter << "，面积 = " << area;
 		return os;
 	}
+	virtual ~Shape() {}
 };
-
 vector<Shape*> addr;     // 存放画过的图形的地址
-
 class Point : public Shape     // 点类
 {
 private:
@@ -53,11 +53,14 @@ public:
 		double dx=x-b.x,dy=y-b.y;
 		return sqrt(dx*dx + dy*dy);
 	}
+	double getX() { return x; }
+	double getY() { return y; }
 	ostream &output(ostream &os = cout)   // 输出函数
 	{
 		os << "( " << x << " , " << y << " )";
 		return os;
 	}
+	~Point() {}
 };
 class Line : public Shape     // 线段类
 {
@@ -110,25 +113,26 @@ public:
 		}
 		if(!equals0(C))   // C为0时不输出
 			os << showpos << C << noshowpos;
-		os << "=0，端点坐标 ";
+		os << "=0，长度 = " << m.dist(n) << "，端点坐标 ";
 		m.output(os) << " ";
 		n.output(os);
 		return os;
 	}
+	~Line() {}
 };
 // make_Line：根据用户输入，生成一条线段并添加其地址到addr中
 void make_Line()
 {
 	system("title Draw a Line");
 	double x1,y1,x2,y2;
-	cout << "请输入线段两个端点的坐标，用空格隔开：" << endl <<endl;
+	cout << "请输入线段两个端点的坐标，用空格隔开：\n" << endl;
 	cout << "第一个点x,y坐标:" << ends;
 	cin >> x1 >> y1;
 	cout << "第二个点x,y坐标:" << ends;
 	cin >> x2 >> y2;
 	while(fabs(x1 - x2) < 1e-8 && fabs(y1 - y2) < 1e-8)     // 两点重合，不能构成线段
 	{
-		cout << "输入的两点重合，不能构成线段。请重新输入：" << endl;
+		cout << "\n输入的两点重合，不能构成线段。请重新输入：" << endl;
 		cout << "第一个点x,y坐标:" << ends;
 		cin >> x1 >> y1;
 		cout << "第二个点x,y坐标:" << ends;
@@ -141,26 +145,253 @@ void make_Line()
 	system("pause");
 	return;
 }
-
+class Circle : public Shape     // 圆类
+{
+private:
+	Point o;    // 圆心
+	double r;   // 半径
+public:
+	Circle() {}
+	Circle(double x_,double y_,double r_):Shape(PI*r_*2,PI*r_*r_),o(x_,y_),r(r_) {}
+	ostream &output(ostream &os = cout)   // 输出函数
+	{
+		os << "圆：方程：" << showpos;
+		if(equals0(o.getX())) os << "x^2";   // 圆心在y轴上，不输出横坐标
+		else os << "(x"  << -o.getX() << ")^2";
+		if(equals0(o.getY())) os << " + y^2";   // 圆心在x轴上，不输出纵坐标
+		else os << " + (y" << -o.getY() << ")^2";
+		os << noshowpos << " = " << r*r << "，圆心 ";
+		o.output(os) << "，半径 r = " << r << "，";
+		Shape::output(os);
+		return os;
+	}
+	~Circle() {}
+};
+// make_Circle：根据用户输入，生成一个圆并添加其地址到addr中
+void make_Circle()
+{
+	system("title Draw a Circle");
+	double x,y,r;
+	cout << "请输入圆心的横纵坐标，用空格隔开:" << ends;
+	cin >> x >> y;
+	cout << "请输入圆的半径:" << ends;
+	cin >> r;
+	while(r<=0)  // 半径必须大于0
+	{
+		cout << "\n输入错误，圆的半径应大于0，请重新输入：\n" << endl;
+		cout << "请输入圆心的横纵坐标，用空格隔开:" << ends;
+		cin >> x >> y;
+		cout << "请输入圆的半径:" << ends;
+		cin >> r;
+	}
+	Circle *c = new Circle(x,y,r);
+	addr.push_back(c);
+	cout << endl << "添加圆成功：" << endl;
+	c->output() << endl;
+	system("pause");
+	return;
+}
 class Ellipse : public Shape     // 椭圆类
 {
 private:
-	double a,b,c,e;     
-	// a长半轴，b短半轴，c焦半径，e离心率
+	double a,b,c,e;   // a长半轴，b短半轴，c焦半径，e离心率
 public:
 	Ellipse() {}
-	Ellipse(double a_,double b_):Shape(),a(a_),b(b_),c(sqrt(a_*a_ - b_*b_))
+	Ellipse(double a_,double b_):Shape(),a(a_),b(b_)
 	{
-		// 椭圆周长无法用初等函数表达，这里略去
+		double p = a>b ? a : b,q = a>b ? b : a;
+		c=sqrt(p*p - q*q);
+		perimeter=PI*(b+a)*(1+3*pow((b-a)/(b+a),2)/(10+sqrt(4-3*pow((b-a)/(b+a),2))));
 		area=PI*a*b;
-		e=c/a;
+		e=c/p;
 	}
 	ostream &output(ostream &os = cout)   // 输出函数
 	{
-
+		os << "椭圆：方程：";
+		os << "x^2";
+		if(!equals0(a-1)) os << "/" << a*a;   // a=1时不输出a^2
+		os << " + y^2";
+		if(!equals0(b-1)) os << "/" << b*b;   // b=1时不输出b^2
+		os << " = 1，长轴长 = " << ( (a>b) ? a*2 : b*2 ) 
+			<< "，短轴长 = " << ( (a>b) ? b*2 : a*2 )
+			<< "，焦距 = " << c*2 << "，离心率 = " << e
+			<< "，周长约为" << perimeter << "，面积 = " << area;
+		return os;
 	}
-
+	~Ellipse() {}
 };
+// make_Ellipse：根据用户输入，生成一个椭圆并添加其地址到addr中
+void make_Ellipse()
+{
+	system("title Draw a Ellipse");
+	double a,b;
+	cout << "请输入椭圆方程的参数：\n" << endl;
+	cout << "参数a:" << ends;
+	cin >> a;
+	cout << "参数b:" << ends;
+	cin >> b;
+	while(a<=0 || b<=0)   // 输入参数非法
+	{
+		cout << "\n输入错误，请重新输入：\n" << endl;
+		cout << "请输入椭圆方程的参数：" << endl;
+		cout << "参数a:" << ends;
+		cin >> a;
+		cout << "参数b:" << ends;
+		cin >> b;
+	}
+	if(equals0(a-b))   // a==b,其实是个圆
+	{
+		cout << "您输入的a,b值相等，请问是否要画个圆？" << endl;
+		cout << "        1 - 画个圆\n        2 - 返回主菜单" << endl;
+		cout << "请选择:" << ends;
+		int choice1;
+		cin >> choice1;
+		if(choice1==1)
+		{
+			Circle *c = new Circle(0,0,a);
+			addr.push_back(c);
+			cout << endl << "添加圆成功：" << endl;
+			c->output() << endl;
+			system("pause");
+		}
+		return;
+	}
+	Ellipse *e = new Ellipse(a,b);
+	addr.push_back(e);
+	cout << endl << "添加椭圆成功：" << endl;
+	e->output() << endl;
+	system("pause");
+	return;
+}
+class Astroid : public Shape     // 星形线
+{
+private:
+	double a;   // 星形线方程唯一的参数
+public:
+	Astroid() {}
+	Astroid(double a_):Shape(a_*6,(PI*a_*a_*3)/8),a(a_) {}
+	ostream &output(ostream &os = cout)   // 输出函数
+	{
+		os << "星形线：参数方程：";
+		os << "x = " << a << " cos^3(t) , y = " << a << " sin^3(t)，";
+		Shape::output(os);
+		return os;
+	}
+	~Astroid() {}
+};
+// make_Astroid：根据用户输入，生成一条星形线并添加其地址到addr中
+void make_Astroid()
+{
+	system("title Draw a Astroid");
+	double a;
+	cout << "请输入星形线的参数a:" << ends;
+	cin >> a;
+	while(a<=0)   // 输入参数非法
+	{
+		cout << "\n输入错误，请重新输入：\n" << endl;
+		cout << "请输入星形线的参数a:" << ends;
+		cin >> a;
+	}
+	Astroid *as = new Astroid(a);
+	addr.push_back(as);
+	cout << endl << "添加星形线成功：" << endl;
+	as->output() << endl;
+	system("pause");
+	return;
+}
+class Cardioid : public Shape     // 心形线
+{
+private:
+	double a;
+public:
+	Cardioid() {}
+	Cardioid(double a_):Shape(a_*8,(PI*a_*a_*3)/2),a(a_) {}
+	ostream &output(ostream &os = cout)   // 输出函数
+	{
+		os << "心形线：极坐标方程：";
+		os << "r = " << a << "(1+cos t)，";
+		Shape::output(os);
+		return os;
+	}
+	~Cardioid() {}
+};
+// make_Cardioid：根据用户输入，生成一条心形线并添加其地址到addr中
+void make_Cardioid()
+{
+	system("title Draw a Cardioid");
+	double a;
+	cout << "请输入心形线的参数a:" << ends;
+	cin >> a;
+	while(a<=0)   // 输入参数非法
+	{
+		cout << "\n输入错误，请重新输入：\n" << endl;
+		cout << "请输入心形线的参数a:" << ends;
+		cin >> a;
+	}
+	Cardioid *c = new Cardioid(a);
+	addr.push_back(c);
+	cout << endl << "添加心形线成功：" << endl;
+	c->output() << endl;
+	system("pause");
+	return;
+}
+class Polygon : public Shape     // 多边形
+{
+private:
+	int nLength;     // 边的数目
+	vector<Point> vertex;     // 多边形的每个顶点
+public:
+	Polygon() {}
+	Polygon(int n,vector<Point> v):Shape(0,0),nLength(n),vertex(v)
+	{
+		perimeter += v[n-1].dist(v[0]);
+		area += v[n-1].getX()*v[0].getY() - v[0].getX()*v[n-1].getY();
+		for(int i=0;i<nLength-1;i++)
+		{
+			perimeter += v[i].dist(v[i+1]);
+			area += v[i].getX()*v[i+1].getY() - v[i+1].getX()*v[i].getY();
+		}
+		area /= 2;
+	}
+	ostream &output(ostream &os = cout)   // 输出函数
+	{
+		os << nLength << "边形：顶点坐标：";
+		for(auto v=vertex.begin();v!=vertex.end();v++)
+			v->output(os) << ",";
+		Shape::output(os);
+		return os;
+	}
+	~Polygon() {}
+};
+// make_Polygon：根据用户输入，生成一个多边形并添加其地址到addr中
+void make_Polygon()
+{
+	system("title Draw a Polygon");
+	int n;
+	double x,y;
+	cout << "您要画几边形:" << ends;
+	cin >> n;
+	while(n<3)   // 输入非法
+	{
+		cout << "\n输入错误，请重新输入：\n" << endl;
+		cout << "您要画几边形:" << ends;
+		cin >> n;
+	}
+	cout << "请按逆时针顺序输入 " << n << " 个顶点：" << endl;
+	vector<Point> v;
+	for(int i=1;i<=n;i++)
+	{
+		cout << "第 " << i << " 个顶点横纵坐标:" << ends;
+		cin >> x >> y;
+		v.push_back(Point(x,y));
+	}
+	Polygon *p = new Polygon(n,v);
+	addr.push_back(p);
+	cout << endl << "添加 " << n << " 边形成功：" << endl;
+	p->output() << endl;
+	system("pause");
+	return;
+}
 // printDirection：输出提示信息
 inline void printDirection()
 {
@@ -175,13 +406,30 @@ inline void printDirection()
 	cout << "        0 - 保存并退出"   << endl;
 	cout << "请选择:" << ends;
 }
+// display：查看已经画过的图形
+void display()
+{
+	system("title Display");
+	if(addr.empty())   // addr为空
+	{
+		cerr << "未添加任何图形。" << endl;
+		system("pause");
+		return;
+	}
+	cout << "共有 " << addr.size() << " 个图形：" << endl << endl;
+	for(auto it=addr.begin(),end=addr.end(); it!=end; ++it)
+		(*it)->output() << endl;
+	cout << endl;
+	system("pause");
+	return;
+}
 // saveAndExit：输出所有图形到指定文件,释放申请的内存
 void saveAndExit()
 {
 	system("title Save and Exit");
 	if(addr.empty())   // addr为空
 	{
-		cerr << "未添加任何图形，无需保存。" << endl;
+		cerr << "未添加任何图形，无需保存。\n" << endl;
 		system("pause");
 		return;
 	}
@@ -190,7 +438,14 @@ void saveAndExit()
 	getline(cin,outFile);
 	while(outFile.empty())
 	{
-		cerr << "文件名不能为空，请重新输入：";
+		cerr << "\n文件名不能为空，请重新输入：";
+		getline(cin,outFile);
+	}
+	regex r("^[^/\\\\:\\*\\?\\<\\>\\|\"]{1,255}$");
+	while(!regex_match(outFile,r))     // 输入的文件名非法
+	{
+		cerr << "\n文件名不能包含下列任何字符  \\ / : * ? \" < > |   也不能超过255个字符" << endl;
+		cerr << "请重新输入:" << ends;
 		getline(cin,outFile);
 	}
 	ofstream fstrm(outFile);
@@ -227,14 +482,14 @@ int main()
 		}
 		switch(choice[0])
 		{
-		case '1':system("cls"); make_Line(); break;   // 画一条线段
-		case '2':system("cls"); break;   // 画一个圆
-		case '3':system("cls"); break;   // 画一个椭圆
-		case '4':system("cls"); break;   // 画个多边形
-		case '5':system("cls"); break;   // 画一条星形线
-		case '6':system("cls"); break;   // 画一条心形线
-		case '7':system("cls"); break;   // 查看已经画过的图形
-		case '0':system("cls"); saveAndExit(); return 0;   // 保存并退出
+		case '1':system("cls"); make_Line();     break;   // 画一条线段
+		case '2':system("cls"); make_Circle();   break;   // 画一个圆
+		case '3':system("cls"); make_Ellipse();  break;   // 画一个椭圆
+		case '4':system("cls"); make_Polygon();  break;   // 画个多边形
+		case '5':system("cls"); make_Astroid();  break;   // 画一条星形线
+		case '6':system("cls"); make_Cardioid(); break;   // 画一条心形线
+		case '7':system("cls"); display();       break;   // 查看已经画过的图形
+		case '0':system("cls"); saveAndExit();   return 0;   // 保存并退出
 
 		default:
 			{
